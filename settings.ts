e56d21d437231707a892670f149a0f833f880b57
Simon Wörpel <simon.woerpel@pm.me>
2025-05-06 09:40:08 +0200

    ✏️ Fix darc names

--- settings.ts
+++ settings.ts
@@ -1,3 +1,3 @@
-export const PAGE_TITLE = "DARC Data Catalog"
+export const PAGE_TITLE = "DARC Library"
 export const CATALOG_URI =
   process.env.CATALOG_URI || "https://data.ftm.store/investigraph.eu.json";
