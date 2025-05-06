e3ef92d69c71eb45dd1f2255465f1644b953d476
Simon Wörpel <simon.woerpel@pm.me>
2025-05-06 09:59:37 +0200

    🔧 Adjust basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withMDX = require("@next/mdx")();
 
+const basePath = process.env.BASE_PATH || "/library";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath,
   trailingSlash: true,
   images: {
     dangerouslyAllowSVG: true,
