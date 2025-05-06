767d5c002c5a3a7c994e664b231eeacebcacdf5f
Simon Wörpel <simon.woerpel@pm.me>
2025-05-06 09:43:08 +0200

    🩹 Add countryNames to StoreContext

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import StoreContext from "~/components/Store";
 import CatalogScreen from "~/screens/CatalogScreen";
 import { CATALOG_URI } from "~/settings";
 import calculateCatalogStats from "~/util/catalogStats";
-import { transformFTMCatalog } from "~/util/transformFTM";
+import { getCountryNames, transformFTMCatalog } from "~/util/transformFTM";
 
 const breadcrumbs = [
   {
@@ -17,11 +17,16 @@ export default async function CatalogPage() {
   const catalog = await getCatalog(CATALOG_URI);
   const { datasets } = transformFTMCatalog(catalog as ICatalog);
   const filterValueCounts = calculateCatalogStats(datasets);
+  const countryNames = getCountryNames(datasets);
 
   return (
     <Page crumbs={breadcrumbs} isRoot>
       <Suspense fallback={"loading..."}>
-        <StoreContext datasets={datasets} filterValueCounts={filterValueCounts}>
+        <StoreContext
+          datasets={datasets}
+          countryNames={countryNames}
+          filterValueCounts={filterValueCounts}
+        >
           <CatalogScreen />
         </StoreContext>
       </Suspense>
