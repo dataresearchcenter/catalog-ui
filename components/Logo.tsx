d7b376475d8c4a1b9fb225249d63864dfc2375ca
Simon Wörpel <simon.woerpel@pm.me>
2025-05-06 10:07:46 +0200

    🍱 Change logo to square version

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,18 +3,22 @@ import Link from "next/link";
 import AspectRatio from "@mui/joy/AspectRatio";
 import Typography from "@mui/joy/Typography";
 import Box from "@mui/system/Box";
-import logoSrc from "~/assets/DARC_basic_light.svg";
+import logoSrc from "~/assets/DARC-Logo-RGB-Square-Triangle-Pos.svg";
 
 export default function Logo() {
   return (
     <Box sx={{ display: "flex" }}>
       <Typography level="body-md">A project by</Typography>
-      <Link target="_blank" rel="noopener" href="https://dataresearchcenter.org/">
+      <Link
+        target="_blank"
+        rel="noopener"
+        href="https://dataresearchcenter.org/"
+      >
         <AspectRatio
-          style={{ width: "61px" }}
+          style={{ paddingLeft: "12px", width: "61px" }}
           variant="plain"
         >
-          <Image fill={true} src={logoSrc} alt="||)·|()" />
+          <Image fill={true} src={logoSrc} alt="DARC" />
         </AspectRatio>
       </Link>
     </Box>
